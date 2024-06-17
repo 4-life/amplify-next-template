ccd6a6282c230d7ae8c3e8cfbf28c5ef20662d89
Pavel <ubuntu@4-life.work>
2024-06-17 16:43:02 +0300

    Fix build

--- lib/types.ts
+++ lib/types.ts
@@ -1,25 +1,3 @@
-import { Comment, Post } from "@/database/dynamo";
-
-export type PostResponse = {
-  error?: string;
-  data?: Post;
-};
-
-export type CommentResponse = {
-  error?: string;
-  data?: Comment;
-};
-
-export type AllPostResponse = {
-  error?: string;
-  data?: Post[];
-};
-
-export type AllCommentsResponse = {
-  error?: string;
-  data?: Comment[];
-};
-
-export interface ReactChildren {
+xport interface ReactChildren {
   children: React.ReactNode
 }
